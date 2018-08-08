687788a42ebe3f1e3c5456a314d77da7e7bd4aff
aclowe <40280424+aclowe@users.noreply.github.com>
2018-08-07 22:49:35 -0500

    Add files via upload
    Modified js file to update and pull values from firebase. Modified background images.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -11,70 +11,72 @@
 
 // Create a variable to reference the database
 var database = firebase.database();
-var trainID= 0
 var trainName = "";
 var destination = "";
-var firstTrainTime = "";
+var firstTrainTime;
 var frequency = "";
 
   // Whenever a user clicks the click button
-$("#submit").on("click", function() {
-    trainID++;
-    trainName = $("#trainName").val();
-    destination = $("#destination").val();
-    firstTrainTime = $("#firstTrainTime").val();
-    frequency = $("#frequency").val();
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    firstTrainTime = $("#firstTrainTime").val().trim();
+    frequency = $("#frequency").val().trim();
     
 
     // Save new value to Firebase
     database.ref().push({
-        trainID: trainID,
         trainName:  trainName,
         destination: destination,
         firstTrainTime:  firstTrainTime,
-        frequency: frequency,
+        frequency: frequency
     });
   
+    //empty text areas
+    $(".inputBox").text("");
+
   });
 
   // At the initial load and on subsequent data value changes, get a snapshot of the current data. 
 // This callback keeps the page updated when a value changes in firebase.
-database.ref().on("value", function(snapshot) {
+database.ref().on("child_added", function(childSnapshot) {
   
     // Console.log the "snapshot" value (a point-in-time representation of the database)
-    console.log(snapshot.val());
+    console.log(childSnapshot.val());
     // This "snapshot" allows the page to get the most current values in firebase.
   
     // Change the value of our clickCounter to match the value in the database
-    trainID = snapshot.val().key;
-    trainName = snapshot.val().trainName;
-    destination = snapshot.val().destination;
-    firstTrainTime = snapshot.val().firstTrainTime;
-    frequency = snapshot.val().frequency;
+    // trainID = childSnapshot.val().key;
+    trainName = childSnapshot.val().trainName;
+    destination = childSnapshot.val().destination;
+    firstTrainTime = childSnapshot.val().firstTrainTime;
+    frequency = childSnapshot.val().frequency;
   
     // Console Log the values
-    console.log("trainID: "  + trainID);
-    console.log("trainName: " + trainName);
-    console.log("destination: " + destination);
-    console.log("firstTrainTime: " + firstTrainTime);
-    console.log("freqency: " + frequency);
+    console.log("trainID: "  + childSnapshot.val().key);
+    console.log("trainName: " + childSnapshot.val().trainName);
+    console.log("destination: " + childSnapshot.val().destination);
+    console.log("firstTrainTime: " + childSnapshot.val().firstTrainTime);
+    console.log("freqency: " + childSnapshot.val().frequency);
   
     // Change the HTML using jQuery to reflect the updated values
-    var newTableRow = $("#trainInfo").append("<tr>")
+    var newTableRow = $("#trainInfo").append("<tr>");
     newTableRow.append("<td><div class='form-check'><input class='form-check-input' type='checkbox' value='' id='defaultCheck1 delete'></div></td>");
-    newTableRow.append("<td>" + trainID + "</td>");
-    newTableRow.append("<td>" + trainName + "</td>");
-    newTableRow.append("<td>" + destination + "</td>");
-    newTableRow.append("<td>" + firstTrainTime + "</td>");
-    newTableRow.append("<td>" + frequency + "</td>");
-
+    newTableRow.append("<td>" + childSnapshot.val().trainName + "</td>");
+    newTableRow.append("<td>" + childSnapshot.val().destination + "</td>");
+    newTableRow.append("<td>" + childSnapshot.val().frequency + "</td>");
+    newTableRow.append("<td>" + childSnapshot.val().nextArrival + "</td>");
+   
+  
     // Change the HTML using jQuery to reflect the updated values
     $("#submit").push(newTableRow)
     // Alternate solution to the above line
     // $("#click-value").html(clickCounter);
   
   // If any errors are experienced, log them to console.
-  }, function(errorObject) {
+  }, 
+  function(errorObject) {
     console.log("The read failed: " + errorObject.code);
   });
   
